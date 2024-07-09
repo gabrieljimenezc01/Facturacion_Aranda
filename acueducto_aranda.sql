8428fb0c060f96c6072ce7ae230ede81275f7a5e
Jhon <jhonhec2002@gmail.com>
2024-07-09 14:06:37 -0500

    Commit prueba JH

--- acueducto_aranda.sql
+++ acueducto_aranda.sql
@@ -6,6 +6,7 @@
 -- Tiempo de generación: 05-07-2024 a las 20:36:09
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
+-- Modificado por JH
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
