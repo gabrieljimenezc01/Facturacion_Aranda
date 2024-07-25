6b23e9a9b3f4ce3d53cf75b6e8be7f0a22d9fc7f
gabrieljimenezc01 <jesusgabrieljimenezceron@gmail.com>
2024-07-25 11:19:53 -0500

    diseño facturas x cliente

--- facturas.php
+++ facturas.php
@@ -32,95 +32,117 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Impresión de Facturas</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="facturas-styles.css">
+    <link rel="stylesheet" href="deudas-styles.css">
 </head>
 
 <body>
-    <div class="filtro">
-        <!-- Formulario de filtrado -->
-        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-            <label>Seleccionar Facturas Por: </label>
-            <div class="form-row">
-                <input type="number" min="1" required name="sector" value='<?php echo $sector ?>'>
-                <label alt="Label" data-placeholder="Sector"></label>
+    <div class="main-container">
+        <nav class="navbar">
+            <div class="navbar-brand">Facturación</div>
+            <div>
+                <button class="logout-button">Cerrar Sesión</button>
             </div>
-            <div class="form-row">
-                <select name="mes" required id="mes">
-                    <option value="<?php echo $mes ?>"><?php echo $mes ?></option>
-                    <option value="ENERO">ENERO</option>
-                    <option value="FEBRERO">FEBRERO</option>
-                    <option value="MARZO">MARZO</option>
-                    <option value="ABRIL">ABRIL</option>
-                    <option value="MAYO">MAYO</option>
-                    <option value="JUNIO">JUNIO</option>
-                    <option value="JULIO">JULIO</option>
-                    <option value="AGOSTO">AGOSTO</option>
-                    <option value="SEPTIEMBRE">SEPTIEMBRE</option>
-                    <option value="OCTUBRE">OCTUBRE</option>
-                    <option value="NOVIEMBRE">NOVIEMBRE</option>
-                    <option value="DICIEMBRE">DICIEMBRE</option>
-                </select>
-            </div>
-            <div class="form-row">
-                <input type="number" min="2000" required name="año" min value='<?php echo $año ?>'>
-                <label alt="Label" data-placeholder="Año de Cobro"></label>
-            </div>
-            <div class="form-row">
-                <button type="submit" name="busqueda">Buscar</button>
-            </div>
-        </form>
-    </div>
-    <div>
-        <table>
-            <tr>
-                <th>Código Factura</th>
-                <th>Código Ususario</th>
-                <th>Nombre Completo</th>
-                <th>uso</th>
-                <th>Sector</th>
-                <th>Fundador</th>
-                <th>Consumo m3</th>
-                <th>Valor Deuda</th>
-                <th>Valor Factura</th>
-                <th>Acciones</th>
-            </tr>
+        </nav>
 
-            <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($result)) {
-                foreach ($result as $row) {
-                    echo "<tr>";
-                    echo "<td>" . htmlspecialchars($row['cod_factura']) . "</td>";
-                    echo "<td>" . htmlspecialchars($row['codigo']) . "</td>";
-                    echo "<td>" . htmlspecialchars($row['nombre'] . " " . $row['apellido']) . "</td>";
-                    echo "<td>" . htmlspecialchars($row['uso']) . "</td>";
-                    echo "<td>" . htmlspecialchars($row['sector']) . "</td>";
-                    echo "<td>" . htmlspecialchars($row['fundador']) . "</td>";
-                    echo "<td>" . htmlspecialchars($row['consumo_m3']) . "</td>";
-                    echo "<td>" . htmlspecialchars($row['valor_deuda']) . "</td>";
-                    echo "<td>" . htmlspecialchars($row['valor_total']) . "</td>";
-                    echo "<td>
+        <div class="content">
+            <aside class="sidebar">
+                <ul class="menu-list">
+                    <li><a href="deudores.php"><i class="fa fa-list" aria-hidden="true"></i><br>Lista Deudores</a></li>
+                    <li><a href="busqueda.php"><i class="fa fa-pencil-square-o" aria-hidden="true"></i><br>Acuerdos de pago</a></li>
+                    <li><a href="clientes_facturas.php"><i class="fa fa-user" aria-hidden="true"></i><br>Facturas de clientes</a></li>
+                </ul>
+            </aside>
+            <main class="main-content">
+
+                <div class="filtro">
+                    <!-- Formulario de filtrado -->
+                    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                        <label>Seleccionar Facturas Por: </label>
+                        <div class="form-row">
+                            <input type="number" min="1" required name="sector" value='<?php echo $sector ?>'>
+                            <label alt="Label" data-placeholder="Sector"></label>
+                        </div>
+                        <div class="form-row">
+                            <select name="mes" required id="mes" class="select">
+                                <option value="<?php echo $mes ?>"><?php echo $mes ?></option>
+                                <option value="ENERO">ENERO</option>
+                                <option value="FEBRERO">FEBRERO</option>
+                                <option value="MARZO">MARZO</option>
+                                <option value="ABRIL">ABRIL</option>
+                                <option value="MAYO">MAYO</option>
+                                <option value="JUNIO">JUNIO</option>
+                                <option value="JULIO">JULIO</option>
+                                <option value="AGOSTO">AGOSTO</option>
+                                <option value="SEPTIEMBRE">SEPTIEMBRE</option>
+                                <option value="OCTUBRE">OCTUBRE</option>
+                                <option value="NOVIEMBRE">NOVIEMBRE</option>
+                                <option value="DICIEMBRE">DICIEMBRE</option>
+                            </select>
+                        </div>
+                        <div class="form-row">
+                            <input type="number" min="2000" required name="año" min value='<?php echo $año ?>'>
+                            <label alt="Label" data-placeholder="Año de Cobro"></label>
+                        </div>
+                        <div class="form-row">
+                            <button type="submit" name="busqueda">Buscar</button>
+                        </div>
+                    </form>
+                </div>
+                <div>
+                    <table>
+                        <tr>
+                            <th>Código Factura</th>
+                            <th>Código Ususario</th>
+                            <th>Nombre Completo</th>
+                            <th>uso</th>
+                            <th>Sector</th>
+                            <th>Fundador</th>
+                            <th>Consumo m3</th>
+                            <th>Valor Deuda</th>
+                            <th>Valor Factura</th>
+                            <th>Acciones</th>
+                        </tr>
+
+                        <?php
+                        if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($result)) {
+                            foreach ($result as $row) {
+                                echo "<tr>";
+                                echo "<td>" . htmlspecialchars($row['cod_factura']) . "</td>";
+                                echo "<td>" . htmlspecialchars($row['codigo']) . "</td>";
+                                echo "<td>" . htmlspecialchars($row['nombre'] . " " . $row['apellido']) . "</td>";
+                                echo "<td>" . htmlspecialchars($row['uso']) . "</td>";
+                                echo "<td>" . htmlspecialchars($row['sector']) . "</td>";
+                                echo "<td>" . htmlspecialchars($row['fundador']) . "</td>";
+                                echo "<td>" . htmlspecialchars($row['consumo_m3']) . "</td>";
+                                echo "<td>" . htmlspecialchars($row['valor_deuda']) . "</td>";
+                                echo "<td>" . htmlspecialchars($row['valor_total']) . "</td>";
+                                echo "<td>
                         <a href='generate_pdf.php?cod_factura=" . htmlspecialchars($row["cod_factura"]) . "' target='_blank'>
                             <i class='fa fa-file-pdf-o' aria-hidden='true'></i>
                         </a>
                     </td>";
-                    echo "</tr>";
-                }
-            } else {
-                echo "<tr><td colspan='10'>No hay registros</td></tr>";
-            }
-            ?>
-        </table>
+                                echo "</tr>";
+                            }
+                        } else {
+                            echo "<tr><td colspan='10'>No hay registros</td></tr>";
+                        }
+                        ?>
+                    </table>
+                </div>
+                <?php if (!empty($result)) : ?>
+                    <div>
+                        <form action="generar_facturas.php" method="post">
+                            <input type="hidden" name="sector" value='<?php echo $sector ?>'>
+                            <input type="hidden" name="mes" value='<?php echo $mes ?>'>
+                            <input type="hidden" name="año" value='<?php echo $año ?>'>
+                            <button type="submit" name="descarga">Descargar facturas</button>
+                        </form>
+                    </div>
+                <?php endif ?>
+            </main>
+        </div>
+
     </div>
-    <?php if (!empty($result)) : ?>
-    <div>
-        <form action="generar_facturas.php" method="post">
-            <input type="hidden" name="sector" value='<?php echo $sector ?>'>
-            <input type="hidden" name="mes" value='<?php echo $mes ?>'>
-            <input type="hidden" name="año" value='<?php echo $año ?>'>
-        <button type="submit" name="descarga">Descargar facturas</button>
-        </form>
-    </div> 
-    <?php endif ?>
 </body>
 
 </html>
